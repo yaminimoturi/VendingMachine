af3c5a92b48f6fde4767479f6bec8bd7b57e257d
Yamini moturi <99952136+yaminimoturi@users.noreply.github.com>
2022-04-23 19:55:46 -0500

    Update OrderBeverageService.cpp

--- src/beverage-preference-service/OrderBeverageService.cpp
+++ src/beverage-preference-service/OrderBeverageService.cpp
@@ -5,7 +5,7 @@
 #include <signal.h>
 
 #include "../utils.h"
-#include "OrderBeverageHandler.h"
+#include "BeveragePreferenceHandler.h"
 
 using json = nlohmann::json;
 using apache::thrift::server::TThreadedServer;
@@ -36,29 +36,19 @@ int main(int argc, char **argv) {
   }
 
   // 3: get my port
-  int my_port = config_json["order-beverage-service"]["port"];
+  int my_port = config_json["beverage-preference-service"]["port"];
 
-  // 4: get the weather service's port and address
-  int weather_service_port = config_json["weather-service"]["port"];
-  std::string weather_service_addr = config_json["weather-service"]["addr"];
- 
-  // 5: get the client of weather-service
-  ClientPool<ThriftClient<WeatherServiceClient>> weather_client_pool(
-      "weather-service", weather_service_addr, weather_service_port, 0, 128, 1000);
-
-  // 6: configure this server
+  // 4: configure this server
   TThreadedServer server(
-      std::make_shared<OrderBeverageServiceProcessor>(
-          std::make_shared<OrderBeverageServiceHandler>(
-              &weather_client_pool)),
+      std::make_shared<BeveragePreferenceServiceProcessor>(
+          std::make_shared<BevereagePreferenceServiceHandler>()),
       std::make_shared<TServerSocket>("0.0.0.0", my_port),
       std::make_shared<TFramedTransportFactory>(),
       std::make_shared<TBinaryProtocolFactory>()
   );
   
-  // 7: start the server
-  std::cout << "Starting the order-beverage server ..." << std::endl;
+  // 5: start the server
+  std::cout << "Starting the BeveragePreference server ..." << std::endl;
   server.serve();
   return 0;
 }
-
